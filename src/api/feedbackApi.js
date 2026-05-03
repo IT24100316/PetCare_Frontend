800acd1875834aae57e76bce6c0652cdbc4fb3ce
IT24102678 <paariskaveesha2002@gmail.com>
2026-05-03 11:53:02 +0530

    Remove deleteFeedback export
    Delete the deleteFeedback function/export from feedbackApi.js

--- src/api/feedbackApi.js
+++ src/api/feedbackApi.js
@@ -17,10 +17,7 @@ export const submitFeedback = async (data) => {
   const response = await axiosInstance.post("/feedbacks", data);
   return response.data;
 };
-export const deleteFeedback = async (id) => {
-  const response = await axiosInstance.delete(`/feedbacks/${id}`);
-  return response.data;
-};
+
 export const updateFeedback = async (id, data) => {
   const response = await axiosInstance.put(`/feedbacks/${id}`, data);
   const data = response.data;
